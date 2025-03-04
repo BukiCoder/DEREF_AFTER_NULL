016c2bca9bf452be343919f94152f157d01534cd
BukiCoder <59802034+BukiCoder@users.noreply.github.com>
2025-03-04 03:27:08 +0300

    Add files via upload

---
+++ Deref_after_null/ValueSuggestion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Deref_after_null
+{
+    internal class ValueSuggestion
+    {
+        public bool n { get; set; }
+        public bool nn { get; set; }
+        public bool beyond { get; set; }
+        public ValueSuggestion(ValueSuggestion oth)
+        {
+            n = oth.n;
+            nn = oth.nn;
+            beyond = oth.beyond;
+        }
+        public ValueSuggestion()
+        {
+            n = false;
+            nn = true;
+            beyond = true;
+        }
+
+    }
+}
